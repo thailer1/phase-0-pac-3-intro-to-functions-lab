6db5ef6171063f5505890309567776d4f0506cc2
Thailer <thailer.palmer@gmail.com>
2023-01-20 16:33:29 -0500

    commit tree 2116ee320598ba5ec725758f54737bf24830c751

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string){
+    return string.toLowerCase();
+  }
+  function logShout (string){}
+  
+  function logWhispher(string){}
+
+  function sayHiToHeadphonedRoommate(){}
+  const string = "Hello";
+
+  console.log(string);
+  console.log(string.toUpperCase());
+
+  cibsike.log(string === string.toUpperCase());
